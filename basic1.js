d4788b0698f4fd0b8dde4cff4d14f24fb3bf7aa6
kindasahil <sahilsadhuramani10@gmail.com>
2023-09-09 12:42:52 +0530

    commit

--- basic1.js
+++ basic1.js
@@ -148,3 +148,119 @@ let gameCounter = 100
 // === 
 
 // console.log("2" === 2); //F
+
+//String
+
+const name1="Sahil"
+const repoCount = 50
+
+// console.log(name1 + repoCount + "hello");
+
+// console.log(`Hello ${name1} count is ${repoCount}.`);
+
+const gameName = String('Hey-Sahil');
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase);
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('y'));
+
+const newString = gameName.substring(0,5);
+// console.log(newString);
+
+const newCheck = gameName.slice(-8,5);
+// console.log(newCheck);
+
+const newStringOne = "     Happy     "
+// console.log(newStringOne.trim());
+
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+// console.log(url.replace('%20', '-'))
+
+// console.log(url.includes('sundar'))
+
+// console.log(gameName.split('-'));
+
+
+const num = 100
+// console.log(num);
+
+const newNum =new Number(200)
+// console.log(newNum);
+
+// console.log(newNum.toString().length);
+// console.log(newNum.toFixed(5));
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score1 = 100
+const scoreValue = 100.3
+
+const isLoggedIn1 = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);//different hai
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]; //Array
+
+//Object {}
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+//Function
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+// myFunction();
+
+// console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
